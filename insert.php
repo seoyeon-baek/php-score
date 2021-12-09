41684b2323b5cb00e5e11a018a0a3d6c74a81b64
seoyeon-baek <w2032@e-mirim.hs.kr>
2021-12-09 16:06:16 +0900

    성적표 추가 구현

--- insert.php
+++ insert.php
@@ -1,1 +1,26 @@
 <!-- 성적 추가 처리 -->
+<?php
+include('./db.php');
+
+$name = '';
+$kor = 0;
+$eng = 0;
+$mat = 0;
+
+$name = $_POST['name'];
+$kor = $_POST['kor'];
+$eng = $_POST['eng'];
+$mat = $_POST['mat'];
+
+if($name != ''){
+  $sql = "INSERT INTO score ( name, kor, eng, mat ) values ( '" . $name . "', " . $kor . ", " . $eng . ", " . $mat . " ); ";
+
+  $result = $mysqli->query($sql);
+  if(!$result){
+    die("쿼리 에러");
+  }
+}
+header('Location: list.php');
+exit;
+// 자바스크립트로도 페이지 리다이렉트 가능 <script>location.href="list.php"</script>
+?>
